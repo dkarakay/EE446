3bc4a49b3c4ece3bcacd0776eef60daf39f85b61
Deniz Karakay <deniz@karakay.me>
2023-05-07 07:05:40 +0300

    Complete 2s complement

--- Experiment_3/Exp3-tests/subroutines/subroutines_cocotb_test.py
+++ Experiment_3/Exp3-tests/subroutines/subroutines_cocotb_test.py
@@ -3,236 +3,192 @@ from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge
 
 
+def print_hex_dec(dut, val, name, cond=False, only_hex=False):
+    if only_hex:
+        my = int(val)
+        dut._log.info(f"{name}: H: {my:x}")
+        return
+
+    if str(val) != "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx":
+        if cond or val != 0:
+            my = int(val)
+            dut._log.info(f"{name}: D: {my:d} H: {my:x} B: {val}")
+
+
+## This function prints all the signals in the DUT except the ones that are zero
+# @param dut: the DUT
 def print_all(dut):
-    dut._log.info(f"INSTR {dut.INSTR.value}")
+    dut._log.info("CYCLE: " + str(dut.CYCLE.value))
+    print_hex_dec(dut, dut.INSTR.value, name="INSTR", only_hex=True)
 
-    my_signal_value = int(dut.INSTR.value)
-    dut._log.info(f"INST (HEX): {my_signal_value:x}")
+    print_hex_dec(dut, dut.RESET.value, name="RESET")
+    print_hex_dec(dut, dut.PCWrite.value, name="PCWrite")
+    print_hex_dec(dut, dut.AdSrc.value, name="AdSrc")
+    print_hex_dec(dut, dut.MemWrite.value, name="MemWrite")
+    print_hex_dec(dut, dut.IRWrite.value, name="IRWrite")
+    print_hex_dec(dut, dut.ResultSrc.value, name="ResultSrc")
+    print_hex_dec(dut, dut.ALUControl.value, name="ALUControl")
+    print_hex_dec(dut, dut.ALUSrcA.value, name="ALUSrcA")
+    print_hex_dec(dut, dut.ALUSrcB.value, name="ALUSrcB")
+    print_hex_dec(dut, dut.ImmSrc.value, name="ImmSrc")
+    print_hex_dec(dut, dut.RegWrite.value, name="RegWrite")
+    print_hex_dec(dut, dut.RegSrc.value, name="RegSrc")
+    print_hex_dec(dut, dut.Sel14.value, name="Sel14")
 
-    dut._log.info(f"PCSrc {dut.PCSrc.value}")
-    dut._log.info(f"RESET {dut.RESET.value}")
-    dut._log.info(f"MemtoReg {dut.MemtoReg.value}")
-    dut._log.info(f"MemWrite {dut.MemWrite.value}")
-    dut._log.info(f"ALUControl {dut.ALUControl.value}")
-    dut._log.info(f"ALUSrc {dut.ALUSrc.value}")
-    dut._log.info(f"ImmSrc {dut.ImmSrc.value}")
-    dut._log.info(f"RegWrite {dut.RegWrite.value}")
-    dut._log.info(f"RegSrc {dut.RegSrc.value}")
+    print_hex_dec(dut, dut.PC.value, name="PC", cond=True)
 
-    dut._log.info(f"------------------")
-    dut._log.info(f"PC {dut.PC.value}")
-    dut._log.info(f"RA1 {dut.RA1.value}")
-    dut._log.info(f"RA2 {dut.RA2.value}")
-    dut._log.info(f"RD1 {dut.RD1.value}")
-    dut._log.info(f"RD2 {dut.RD2.value}")
-    dut._log.info(f"OUT {dut.OUT.value}")
-    print('------------------')
+    print_hex_dec(dut, dut.FlagZ.value, name="FlagZ")
+    print_hex_dec(dut, dut.WD3.value, name="WD3")
+    print_hex_dec(dut, dut.RA1.value, name="RA1")
+    print_hex_dec(dut, dut.RA2.value, name="RA2")
+    print_hex_dec(dut, dut.RA3.value, name="RA3")
+    print_hex_dec(dut, dut.RD1.value, name="RD1")
+    print_hex_dec(dut, dut.RD2.value, name="RD2")
+    print_hex_dec(dut, dut.ALUResult.value, name="ALUResult")
+    print_hex_dec(dut, dut.ALUOut.value, name="ALUOut")
+
+    print_hex_dec(dut, dut.OUT.value, name="OUT", cond=True)
+    print("------------------")
 
 
-'''
+# Fetch and decode cycles
+async def fetch_and_decode(dut, clkedge, skip_print=False):
+    # Cycle 1 - Fetch
+
+    await clkedge
+    if not skip_print:
+        dut._log.info(f"Cycle 1 - Fetch")
+        print_all(dut)
+    # assert dut.ALUResult.value == dut.PC.value + 4
+    # assert dut.OUT.value == dut.ALUResult.value
+
+    # Cycle 2 - Decode
+    await clkedge
+    if not skip_print:
+        dut._log.info(f"Cycle 2 - Decode")
+        print_all(dut)
+    # assert dut.ALUResult.value == dut.PC.value + 4
+    # assert dut.OUT.value == dut.ALUResult.value
+
+
 @cocotb.test()
-async def subroutine_2s_complement(dut):
+async def main_cocotb_test(dut):
     """Setup testbench and run a test."""
     # Generate the clock
-    await cocotb.start(Clock(dut.CLK, 10, 'us').start(start_high=False))
+    await cocotb.start(Clock(dut.CLK, 10, "us").start(start_high=False))
 
     # set clkedge as the rising edge of the clock
     clkedge = RisingEdge(dut.CLK)
     # wait until the clock edge
-
     dut.RESET.value = 1
     await clkedge
-
-    dut.RESET.value = 0
-    # print_all(dut)
-
-    # LDR R3, R0 => Load from memory location 0 to R3 <== 0x0000000A
-    # INSTR = 0xE4103000
-
-    await clkedge
-    dut._log.info(f"LDR R3, R0 => Load from memory location 0 to R3 <== 0x0000000A")
-    print_all(dut)
-    assert dut.INSTR.value == 0xE4103000
-    assert dut.PCSrc.value == 0
-    assert dut.MemtoReg.value == 1
-    assert dut.MemWrite.value == 0
-    assert dut.ALUControl.value == 4
-    assert dut.ALUSrc.value == 1
-    assert dut.ImmSrc.value == 1
-    assert dut.RegWrite.value == 1
-    assert dut.RegSrc.value == 0
-
-    # SUB R5,R2,R3 => R2 - R3 and store in R5 R2=0 and R3=0x0000000A
-    # INSTR = 0xE0425003
-    PCSrc = 0
-    MemtoReg = 0
-    MemWrite = 0
-    ALUControl = 2
-    ALUSrc = 0
-    ImmSrc = 0
-    RegWrite = 1
-    RegSrc = 0
-
-    await clkedge
-    dut._log.info(f"SUB R5,R2,R3 => R2 - R3 and store in R5")
-    print_all(dut)
-    assert dut.INSTR.value == 0xE0425003
-    assert dut.PCSrc.value == PCSrc
-    assert dut.MemtoReg.value == MemtoReg
-    assert dut.MemWrite.value == MemWrite
-    assert dut.ALUControl.value == ALUControl
-    assert dut.ALUSrc.value == ALUSrc
-    assert dut.ImmSrc.value == ImmSrc
-    assert dut.RegWrite.value == RegWrite
-    assert dut.RegSrc.value == RegSrc
-
-    # STR R5,[R0,#8] => Store R5 to memory location 8
-    # INSTR = 0xE4005008
-    PCSrc = 0
-    MemtoReg = 0
-    MemWrite = 1
-    ALUControl = 4
-    ALUSrc = 1
-    ImmSrc = 1
-    RegWrite = 0
-    RegSrc = 2
-
-    await clkedge
-    dut._log.info(f"STR R5,[R0,#8] => Store R5 to memory location 8")
-    print_all(dut)
-    assert dut.INSTR.value == 0xE4005008
-    assert dut.PCSrc.value == PCSrc
-    assert dut.MemtoReg.value == MemtoReg
-    assert dut.MemWrite.value == MemWrite
-    assert dut.ALUControl.value == ALUControl
-    assert dut.ALUSrc.value == ALUSrc
-    assert dut.ImmSrc.value == ImmSrc
-    assert dut.RegWrite.value == RegWrite
-    assert dut.RegSrc.value == RegSrc
-
-    # LDR R2, R0, #8 => Load from memory location 8 to R3 <== 2's complement of 0x0000000A
-    # INSTR = 0xE4102008
-    PCSrc = 0
-    MemtoReg = 1
-    MemWrite = 0
-    ALUControl = 4
-    ALUSrc = 1
-    ImmSrc = 1
-    RegWrite = 1
-    RegSrc = 0
-
-    await clkedge
-    dut._log.info(f"LDR R2, R0, #8 => Load from memory location 8 to R3 <== 2's complement of 0x0000000A")
-    print_all(dut)
-    assert dut.INSTR.value == 0xE4102008
-    assert dut.PCSrc.value == PCSrc
-    assert dut.MemtoReg.value == MemtoReg
-    assert dut.MemWrite.value == MemWrite
-    assert dut.ALUControl.value == ALUControl
-    assert dut.ALUSrc.value == ALUSrc
-    assert dut.ImmSrc.value == ImmSrc
-    assert dut.RegWrite.value == RegWrite
-    assert dut.RegSrc.value == RegSrc
-'''
-
-
-@cocotb.test()
-async def subroutine_2s_complement_using_branch(dut):
-    """Setup testbench and run a test."""
-    # Generate the clock
-    await cocotb.start(Clock(dut.CLK, 10, 'us').start(start_high=False))
-
-    # set clkedge as the rising edge of the clock
-    clkedge = RisingEdge(dut.CLK)
-    # wait until the clock edge
-
     dut.RESET.value = 1
     await clkedge
-
+    print_all(dut)
     dut.RESET.value = 0
-    # print_all(dut)
 
-    # Branch to subroutine at address 0x00000100
-    # INSTR = 0xEA000004
+    # Check BL
+    # Address 60
+    # BL 0x00000002 -> PC = 0 + 8 + 2*4 = 16 R14 = 4
+    # 0xEB000002
 
-    await clkedge
-    dut._log.info(f"Branch to subroutine at address 0x00000010")
-    print_all(dut)
+    dut._log.info(f"BL 0x00000002 -> PC = 0 + 8 + 2*4 = 16 R14 = 4")
+    # Fetch and decode
+    await fetch_and_decode(dut, clkedge)
 
-    await clkedge
-    dut._log.info(f"LDR R3, R0 => Load from memory location 0 to R3 <== 0x0000000A")
-    print_all(dut)
-
-    # SUB R5,R2,R3 => R2 - R3 and store in R5 R2=0 and R3=0x0000000A
-    # INSTR = 0xE0425003
-    PCSrc = 0
-    MemtoReg = 0
-    MemWrite = 0
-    ALUControl = 2
-    ALUSrc = 0
-    ImmSrc = 0
-    RegWrite = 1
-    RegSrc = 0
+    # Cycle 3 - Branch
 
     await clkedge
-    dut._log.info(f"SUB R5,R2,R3 => R2 - R3 and store in R5")
+    dut._log.info(f"Cycle 3 - Branch")
     print_all(dut)
-    assert dut.INSTR.value == 0xE0425003
-    assert dut.PCSrc.value == PCSrc
-    assert dut.MemtoReg.value == MemtoReg
-    assert dut.MemWrite.value == MemWrite
-    assert dut.ALUControl.value == ALUControl
-    assert dut.ALUSrc.value == ALUSrc
-    assert dut.ImmSrc.value == ImmSrc
-    assert dut.RegWrite.value == RegWrite
-    assert dut.RegSrc.value == RegSrc
+    assert dut.INSTR.value == 0xEB000002
 
-    # STR R5,[R0,#8] => Store R5 to memory location 8
-    # INSTR = 0xE4005008
-    PCSrc = 0
-    MemtoReg = 0
-    MemWrite = 1
-    ALUControl = 4
-    ALUSrc = 1
-    ImmSrc = 1
-    RegWrite = 0
-    RegSrc = 2
+    # Cycle 4 - Branch
+    await clkedge
+    dut._log.info(f"Cycle 4 - Branch")
+    print_all(dut)
+    assert dut.PC.value == 16
+
+    # Cycle 5 - Branch
+    await clkedge
+    dut._log.info(f"Cycle 5 - Branch")
+    print_all(dut)
+
+    # Check LDR
+    # Address 16
+    # LDR R3, [R0, #124] => 7
+    # 0xE410307C
+
+    dut._log.info(f"LDR R3, [R0, #124] => 7")
+
+    # Fetch and decode
+    await fetch_and_decode(dut, clkedge)
+
+    # Cycle 3 - MemAddr
+    await clkedge
+    dut._log.info(f"Cycle 3 - MemAddr")
+    print_all(dut)
+    assert dut.ALUResult.value == 124
+    assert dut.OUT.value == dut.PC.value + 4
+
+    # Cycle 4 - MemRead
+    await clkedge
+    dut._log.info(f"Cycle 4 - MemRead")
+    print_all(dut)
+    assert dut.ALUResult.value == 124
+
+    # Cycle 5 - WriteBack
+    await clkedge
+    dut._log.info(f"Cycle 5 - WriteBack")
+    print_all(dut)
+    assert dut.OUT.value == 7
+
+    # Check SUB operation
+    # Address 20
+    # SUB R5, R0, R3 -> R5 = 0-7 = -7 in 2's complement = 0xFFFFFFF9
+    # 0xE0405003
+    dut._log.info(f"SUB R5, R0, R3 -> R5 = 0-7 = -7 in 2's complement = 0xFFFFFFF9")
+
+    # Fetch and decode cycles
+    await fetch_and_decode(dut, clkedge, skip_print=True)
+
+    # Cycle 3 - Execute
+    await clkedge
+    dut._log.info(f"Cycle 3 - Execute")
+    print_all(dut)
+    assert dut.INSTR.value == 0xE0405003
+
+    # Cycle 4 - ALUWriteBack
+    await clkedge
+    dut._log.info(f"Cycle 4 - ALUWriteBack")
+    print_all(dut)
+    assert dut.OUT.value == 0xFFFFFFF9
+
+    # Cycle 5 - Wait
+    await clkedge
+    dut._log.info(f"Cycle 5 - Wait")
+    print_all(dut)
+
+    # Check BX LR
+    # Address 24
+    # BX LR -> PC = 4
+    # 0xE800000E
+
+    dut._log.info(f"BX LR -> PC = 4")
+
+    # Fetch and decode
+    await fetch_and_decode(dut, clkedge)
 
     await clkedge
-    dut._log.info(f"STR R5,[R0,#8] => Store R5 to memory location 8")
+    dut._log.info(f"Cycle 3 - Execute")
     print_all(dut)
-    assert dut.INSTR.value == 0xE4005008
-    assert dut.PCSrc.value == PCSrc
-    assert dut.MemtoReg.value == MemtoReg
-    assert dut.MemWrite.value == MemWrite
-    assert dut.ALUControl.value == ALUControl
-    assert dut.ALUSrc.value == ALUSrc
-    assert dut.ImmSrc.value == ImmSrc
-    assert dut.RegWrite.value == RegWrite
-    assert dut.RegSrc.value == RegSrc
-    assert dut.RD2.value == 0xFFFFFFF4
-
-    # LDR R2, R0, #8 => Load from memory location 8 to R3 <== 2's complement of 0x0000000A
-    # INSTR = 0xE4102008
-    PCSrc = 0
-    MemtoReg = 1
-    MemWrite = 0
-    ALUControl = 4
-    ALUSrc = 1
-    ImmSrc = 1
-    RegWrite = 1
-    RegSrc = 0
+    assert dut.INSTR.value == 0xE800000E
 
     await clkedge
-    dut._log.info(f"LDR R2, R0, #8 => Load from memory location 8 to R3 <== 2's complement of 0x0000000A")
+    dut._log.info(f"Cycle 4 - Wait")
     print_all(dut)
-    assert dut.INSTR.value == 0xE4102008
-    assert dut.PCSrc.value == PCSrc
-    assert dut.MemtoReg.value == MemtoReg
-    assert dut.MemWrite.value == MemWrite
-    assert dut.ALUControl.value == ALUControl
-    assert dut.ALUSrc.value == ALUSrc
-    assert dut.ImmSrc.value == ImmSrc
-    assert dut.RegWrite.value == RegWrite
-    assert dut.RegSrc.value == RegSrc
-    assert dut.OUT.value == 0xFFFFFFF4
+    assert dut.PC.value == 4
+
+    await clkedge
+    dut._log.info(f"Cycle 5 - Wait")
+    print_all(dut)
